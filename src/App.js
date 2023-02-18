f78fc33e7ac5b0d3a6b84e957e12732061f049a3
jsk28 <jsk28@sussex.ac.uk>
2023-02-18 18:51:15 +0000

    first thing working

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [githubData, setGithubData] = useState([])
+  const [githubData, setGithubData] = useState({})
   const [githubUser, setGithubUser] = useState("jsk28")
   const [repo, setGithubRepo] = useState("save-titanic")
 
+  
+  const fetchData = async () => {
+    console.log(githubData.size)
+    return fetch(`https://api.github.com/repos/${githubUser}/${repo}`)
+    .then((response) => response.json())
+    .then((data) => setGithubData(data));
+  }
+  
   useEffect(() => {
     fetchData()
   }, [])
-
-  const fetchData = () => {
-    return fetch(`https://api.github.com/${githubUser}/${repo}`)
-      .then((response) => response.json())
-      .then((data) => setGithubData(data));
-
-  }
-
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -33,8 +34,8 @@ function App() {
         <input type="text" placeholder="User" onChange={(e) => setGithubUser(e.target.value)} className="input_search" />
         <input type="text" placeholder="Repo" onChange={(e) => setGithubRepo(e.target.value)} className="input_search" />
         <button onClick={fetchData} className="search_button">Search Github</button>
-
         <br></br>
+        <p>{githubData.size}</p>
 
         <a
           className="App-link"
