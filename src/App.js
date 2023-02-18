6fc75a5b2e8d93eb2ed26a41badeb19f8dac4b2d
jsk28 <jsk28@sussex.ac.uk>
2023-02-18 18:21:47 +0000

    basic input

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import logo from './ship.png';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [githubData, setGithubData] = useState([])
+  const [githubUser, setGithubUser] = useState("jsk28")
+  const [repo, setGithubRepo] = useState("save-titanic")
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    return fetch(`https://api.github.com/${githubUser}/${repo}`)
+      .then((response) => response.json())
+      .then((data) => setGithubData(data));
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <p>
+        <p>
           Save the Titanic!
-      </p>
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
 
 
-      <br></br>
+        <br></br>
 
-        <label for="url-input"></label>
-        <input type="text" id="url-input" name="url"></input>
+        <input type="text" placeholder="User" onChange={(e) => setGithubUser(e.target.value)} className="input_search" />
+        <input type="text" placeholder="Repo" onChange={(e) => setGithubRepo(e.target.value)} className="input_search" />
+        <button onClick={fetchData} className="search_button">Search Github</button>
 
         <br></br>
-       
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
 
-          
+
         >
           Connect your Github account
         </a>
@@ -34,3 +52,4 @@ function App() {
 }
 
 export default App;
+
