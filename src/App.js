5d5a83271a82d950d7a82fe7f3c32c71ecd0dcac
Gregoire de Marcellus <gdemarcellus@gmail.com>
2023-02-18 19:26:29 +0000

    added the calculators to App.js
    multiple calculators  are done self explanatory

--- src/App.js
+++ src/App.js
@@ -32,5 +32,30 @@ function App() {
     </div>
   );
 }
+//assume the input is in kilo byte
+function calculator_kilowhatPerhour(fileSize) {
+  const kiloWhatperHourperGigabyte = 0.01;
+  let gigaByte = fileSize/1024;
+  let kiloWhatperHour = gigaByte * kiloWhatperHourperGigabyte;
+  return kiloWhatperHour;
+}
+//assume the input is in kilo byte
+function calculator_carbonEmision(fileSize) {
+  const CarbonPerKiloWhatPerHour = 0.309;
+  let CarbonperEmission = calculator_kilowhatPerhour(fileSize) * CarbonPerKiloWhatPerHour; 
+  return CarbonperEmission;
+}
+//assume the input is in kilo byte
+function calculator_finacialCost(fileSize){
+const pricePerKiloWhat = 0.32;
+let finacialCost = calculator_kilowhatPerhour(fileSize) * pricePerKiloWhat;
+return finacialCost
+}
+//assume the input is in kilo byte
+function calculator_treePlant(fileSize){
+  const treeperKiloByte = 0.0026;
+  let treeCost = treeperKiloByte * fileSize;
+  return treeCost;
+}
 
 export default App;
